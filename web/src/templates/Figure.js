b9197940df2d1034ea1f80404f871e112d3abb15
simonachkar <contact@simonachkar.com>
2019-06-22 13:52:18 -0300

    Updated FIgure styles

--- web/src/templates/Figure.js
+++ web/src/templates/Figure.js
@@ -3,19 +3,22 @@ import Img from 'gatsby-image'
 import styled from 'styled-components'
 import { getFluidGatsbyImage } from 'gatsby-source-sanity'
 import clientConfig from '../../client-config'
+import { sizes } from '../utils/global'
 
 const Figure = styled.div`
-  padding: 2rem;
-  width: 10rem;
+  padding: 2rem 10%;
   display: block;
   margin-left: auto;
   margin-right: auto;
+  @media (max-width: ${sizes.medium}) {
+    padding: 2rem 1%;
+  }
 `
 
 export default ({ img }) => {
   const fluidProps = getFluidGatsbyImage(
     img.node.asset._ref,
-    { maxWidth: 50 },
+    { maxWidth: 600 },
     ...clientConfig.sanity
   )
   return (
