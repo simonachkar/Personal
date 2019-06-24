96c9c6ae536c7b1f2579fc4c7e10233eeb910371
simonachkar <contact@simonachkar.com>
2019-06-24 14:17:26 -0300

    Added contact page mockup

---
+++ web/src/components/Contact.js
@@ -0,0 +1,116 @@
+import React from 'react'
+import styled from 'styled-components'
+import { graphql, useStaticQuery, Link } from 'gatsby'
+
+import Img from 'gatsby-image'
+
+import { colors, sizes } from '../utils/global'
+
+const margin = '3%'
+const marginOnFocus = '1%'
+
+const Form = styled.form`
+  display: flex;
+  font-weight: 500;
+  flex-direction: column;
+  margin: 0 20% 3rem 20%;
+  text-transform: uppercase;
+`
+
+const Input = styled.input`
+  outline: 0;
+  border: 1px #eee solid;
+  border-radius: 2rem;
+  padding: 1rem;
+  margin-left: ${margin};
+  margin-right: ${margin};
+  margin-bottom: 0.5rem;
+  &:focus {
+    margin-left: ${marginOnFocus};
+    margin-right: ${marginOnFocus};
+    animation: strech 0.8s;
+    @keyframes strech {
+      from {
+        margin-left: ${margin};
+        margin-right: ${margin};
+      }
+      to {
+        margin-left: ${marginOnFocus};
+        margin-right: ${marginOnFocus};
+      }
+    }
+  }
+`
+
+const TextArea = styled.textarea`
+  outline: 0;
+  border: 1px #eee solid;
+  border-radius: 1rem;
+  padding: 1rem;
+  margin-left: ${margin};
+  margin-right: ${margin};
+  margin-bottom: 0.5rem;
+  &:focus {
+    margin-left: ${marginOnFocus};
+    margin-right: ${marginOnFocus};
+    animation: strech 0.8s;
+    @keyframes strech {
+      from {
+        margin-left: ${margin};
+        margin-right: ${margin};
+      }
+      to {
+        margin-left: ${marginOnFocus};
+        margin-right: ${marginOnFocus};
+      }
+    }
+  }
+`
+
+const Button = styled.button`
+  background-color: ${colors.primary};
+  cursor: pointer;
+  outline: 0;
+  text-transform: uppercase;
+  color: white;
+  border: none;
+  border-radius: 2rem;
+  margin: 1rem 15rem;
+  animation: hoverOut 0.5s;
+  padding: 0.5rem;
+
+  &:hover {
+    margin: 1rem 14rem;
+    animation: hoverIn 0.5s;
+  }
+
+  @keyframes hoverOut {
+    from {
+      margin: 1rem 14rem;
+    }
+    to {
+      margin: 1rem 15rem;
+    }
+  }
+
+  @keyframes hoverIn {
+    from {
+      margin: 1rem 15rem;
+    }
+    to {
+      margin: 1rem 14rem;
+    }
+  }
+`
+
+export default () => {
+  return (
+    <Form>
+      <Input type="text" name="name" placeholder="Full Name"></Input>
+      <Input type="text" name="email" placeholder="E-mail"></Input>
+      <Input type="text" name="subject" placeholder="Phone"></Input>
+      <TextArea rows="5" name="message" placeholder="Your Message"></TextArea>
+      <Button>Send</Button>
+    </Form>
+  )
+}
--- web/src/pages/contact.js
+++ web/src/pages/contact.js
@@ -1,11 +1,24 @@
 import React from 'react'
 
 import Header from '../components/Header'
+import Footer from '../components/Footer'
+import Contact from '../components/Contact'
+import { PageTitle } from '../components/Shared'
 
 export default () => {
   return (
     <div>
       <Header page="contact" />
+      <PageTitle>Send me a message</PageTitle>
+      <div style={{ margin: '1rem 10%', paddingBottom: '1rem' }}>
+        <p>
+          If you have an interesting weekend project for me, have a question or
+          comment on one of my blog posts, or just want to say hi. Feel free to
+          contact me. I am also available for hire!
+        </p>
+      </div>
+      <Contact />
+      <Footer />
     </div>
   )
 }
